4d5a21510345aa2432c6d77af585549e2e762ae2
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-07-25 11:48:25 +0530

    working with Functional  Component

--- dailytask/src/Component/CreateTodo.jsx
+++ dailytask/src/Component/CreateTodo.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
-const CreateTodo = () => {
-
-  const task = 0; // variable 
-  const createTask = () => { // function 
-    // if (task === 0){
-    //   return " No Task available "
-    // } else {
-    //   return `${task}`;
-    // }
-
-    return task === 0 ? " No Task available " : ` Total Task : ${task}`; // Ternery Operator
-
-    
+function CreateTodo() {
+ 
+  const imageUrl= "user.png";
+  const isDisable = true;
+  function handleClick (){
+    alert("Button clicked!!");
   }
-  
-  return (              // Functional Componenet me eske return ke uper Java Script ka code likhte hain & return  ke niche HTML me {} ke under Java Script code ko use krte he
-    <div>
-    <h2> Task:{createTask()} </h2>
-    <button> Add Task </button>
-      <h1>Hello CreateTodo </h1>
-    </div>
+
+   
+  return (
+    <>
+      
+      <img src = {imageUrl} alt = "user Photo "></img>
+      <button disable = {isDisable}>Submit</button>
+
+      <button onClick={handleClick}>ClickMe</button>
+
+
+     </>
   )
 }
 
